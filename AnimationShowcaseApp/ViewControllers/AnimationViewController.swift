5d25a8f0a256db7746ebcbfbc33cd1524f35dad8
kuznet.i.sova <kis7425910@gmail.com>
2020-06-17 01:24:19 +0300

    added logic

--- AnimationShowcaseApp/ViewControllers/AnimationViewController.swift
+++ AnimationShowcaseApp/ViewControllers/AnimationViewController.swift
@@ -28,45 +28,44 @@ class AnimationViewController: UIViewController {
 
     @IBAction func changeAnimation() {
         switch changeAnimationButton.titleLabel?.text {
-                case "Start":
-                    infoAboutAnimationLebel.text =
-                    """
-                    Animation: flash
-                    Curve: easeIn
-                    Force: 1
-                    Delay: 1
-                    Duration: 0.5
-                    """
+            case "Start":
+                infoAboutAnimationLebel.text =
+                """
+                Animation: flash
+                Curve: easeIn
+                Force: 1
+                Delay: 1
+                Duration: 0.5
+                """
                     
-                    animationView.animation = "flash"
-                    animationView.curve = "easeIn"
-                    animationView.force = 1
-                    animationView.delay = 1
-                    animationView.duration =  0.5
-                    animationView.animate()
+                animationView.animation = "flash"
+                animationView.curve = "easeIn"
+                animationView.force = 1
+                animationView.delay = 1
+                animationView.duration =  0.5
+                animationView.animate()
                     
-//                    changeAnimationButton.titleLabel?.text = "slideUp"
-                case "slideUp":
-                    infoAboutAnimationLebel.text =
-                    """
-                    Animation: slideUp
-                    Curve: linear
-                    Force: 2.7
-                    Delay: 0.5
-                    Duration: 1.7
-                    """
+                changeAnimationButton.setTitle("slideUp", for: .normal)
+            case "slideUp":
+                infoAboutAnimationLebel.text =
+                """
+                Animation: slideUp
+                Curve: linear
+                Force: 2.7
+                Delay: 0.5
+                Duration: 1.7
+                """
                     
-                    animationView.animation = "slideUp"
-                    animationView.curve = "linear"
-                    animationView.force = 2.7
-                    animationView.delay = 0.5
-                    animationView.duration =  1.7
-                    animationView.animate()
+                animationView.animation = "slideUp"
+                animationView.curve = "linear"
+                animationView.force = 2.7
+                animationView.delay = 0.5
+                animationView.duration =  1.7
+                animationView.animate()
                     
-//                    changeAnimationButton.titleLabel?.text = "Start"
-                default:
-                    changeAnimationButton.titleLabel?.text = "Start"
-                }
+                changeAnimationButton.setTitle("Start", for: .normal)
+            default: break
+            }
     }
 }
 
